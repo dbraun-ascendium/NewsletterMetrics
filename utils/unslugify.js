8d4623f4c84c7cdda5a9125b65c13fc75dada2f9
Dan Braun <danbraun@users.noreply.github.com>
2023-03-23 06:31:54 -0500

    Update unslugify to remove trailing slash

--- utils/unslugify.js
+++ utils/unslugify.js
@@ -1,9 +1,14 @@
-const unslugify = (slug) =>
-slug
-  .replace(/\-/g, " ")
-  .replace(
-    /\w\S*/g,
-    (text) => text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
-  )
+const unslugify = (slug) => {
+  let result = slug
+    .replace(/\-/g, " ")
+    .replace(
+      /\w\S*/g,
+      (text) => text.charAt(0).toUpperCase() + text.slice(1).toLowerCase()
+    )
+  if (result.charAt(result.length - 1) === "/") {
+    result = result.slice(0, result.length - 1)
+  }
+  return result
+}
 
-  module.exports = unslugify
+module.exports = unslugify
